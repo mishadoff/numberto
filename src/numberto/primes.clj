3dec7a1d1b87d2c23437493a52064d9599ec2ff6
mishadoff <mishadoff@gmail.com>
2013-05-14 18:42:24 +0300

    Basic prime numbers added

--- src/numberto/primes.clj
+++ src/numberto/primes.clj
@@ -1,3 +1,14 @@
 (ns numberto.primes)
 
 ;; Prime numbers
+(defn primes []
+  (letfn [(next-prime [p ps]
+            (cond (some #(zero? (mod p %)) (take-while #(<= (* % %) p) ps))
+                  (recur (+ p 2) ps)
+                  :else (cons p (lazy-seq (next-prime (+ p 2) (conj ps p))))))]
+    (cons 2 (lazy-seq (next-prime 3 [])))))
+
+(defn prime? [p]
+  (and (> p 1)
+       (not (some #(zero? (mod p %))
+                  (take-while #(<= (* % %) p) (range 2 p))))))
