71e64a0d52eb3262267683558cb3f9fb0b6a1854
mishadoff <mishadoff@gmail.com>
2013-08-03 00:01:05 +0300

    Simple validator added

---
+++ src/numberto/validator.clj
@@ -0,0 +1,12 @@
+(ns numberto.validator)
+
+(def validate-predicates 
+  {:integer integer?
+   :number number?
+   :positive pos?
+   :non-negative (comp not neg?)})
+
+(defn validate [num rules]
+  (doseq [r rules]
+    (if (not ((get validate-predicates r) num))
+      (throw (IllegalArgumentException. (str "num must satisfy predicate " r))))))
