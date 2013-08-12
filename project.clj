3fe6122fa277aed9fa68be99693a052b99927731
mishadoff <mishadoff@gmail.com>
2013-08-13 00:38:41 +0300

    Pushed to clojars

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject numberto "0.1"
+(defproject mishadoff/numberto "0.0.1-SNAPSHOT"
   :description "numberto - all you need to play with numbers"
-  :url "TBD"
+  :url "https://github.com/mishadoff/numberto"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]])
