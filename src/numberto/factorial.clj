7dd503db2219447ab851861bc2c73673df1ef7ab
mishadoff <mishadoff@gmail.com>
2013-08-13 17:53:33 +0300

    Factorial refactoring

--- src/numberto/factorial.clj
+++ src/numberto/factorial.clj
@@ -4,24 +4,22 @@
   (:require [numberto.math :as m]))
 
 (defn ! [n]
-  "Standard factorial version"
+  "Standard factorial function. Returns 1 * 2 * 3 * ... * n"
   (v/validate n :integer :non-negative)
   (loop [cur n acc 1]
     (if (zero? cur) acc
         (recur (dec cur) (*' cur acc)))))
 
-(defn- find-power [n k]
-  "Return the power of factorization prime number k for number n!"
-  (loop [total n sum 0]
-    (let [i (int (/ total k))]
-      (if (zero? i) sum
-          (recur i (+ sum i))))))
-
 (defn !! [n]
   (v/validate n :integer :non-negative)
-  "Improved version of factorial by factorization"
-  (loop [[h & t] 
-         (map #(m/power* % (find-power n %))
-              (take-while #(<= % n) (s/primes)))
-         acc 1]
-    (if h (recur t (*' h acc)) acc)))
+  "Improved version of factorial by factorization. Works better for large numbers."
+  (letfn [(find-power [n k]
+            (loop [total n sum 0]
+              (let [i (int (/ total k))]
+                (if (zero? i) sum
+                    (recur i (+ sum i))))))]
+    (loop [[h & t] 
+           (map #(m/power* % (find-power n %))
+                (take-while #(<= % n) (s/primes)))
+           acc 1]
+      (if h (recur t (*' h acc)) acc))))
