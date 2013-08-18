8bb401b93cc9e8de8c5099b28fb96a4bddd064b5
mishadoff <mishadoff@gmail.com>
2013-08-18 14:50:14 +0300

    number-name function added

--- doc/features.md
+++ doc/features.md
@@ -1,5 +1,40 @@
 Features to be added
 
+# Printers [NEW]
+
+### Long numbers
+
+If you have a *really* big number, printing it to `System.out` not a good idea.
+`format-num` allows you to capture important property of number, and do not foul up console. 
+
+``` clojure
+(format-num 27647234687347823658723657823) => "27647...[19]...57823"
+```
+
+It prints 5 first digits, 5 last digits and a number of digits between. By the way
+it is configurable.
+
+``` clojure
+(format-num 27647234687347823658723657823 {:s 1 :e 1 :cnt false}) => "2...3"
+```
+
+Have a problem to read long number? Use following method:
+
+``` clojure
+(number-name 16532561257523723757234781264) =>
+"sixteen octillion five hundred thirty two septillion five hundred sixty one sextillion
+two hundred fifty seven quintillion five hundred twenty three quadrillion seven hundred
+twenty three trillion seven hundred fifty seven billion two hundred thirty four million
+seven hundred eighty one thousand two hundred sixty four"
+```
+
+Though we have `format-num` function that captures some number properties, you
+might want to select `what` properties are important for you:
+
+``` clojure
+(what 235390) => {:number 235390, :count-of-digits 6, :sum-of-digits 22}
+```
+
 # Converters
 
 ### Roman numbers
@@ -11,10 +46,6 @@ Features to be added
 ``` clojure
 (roman->number "MCMLXXXIX") => 1989
 ```
-
-### Binary systems (more than 36)
-
-[TODO]
 
 # Factorial
 
@@ -39,7 +70,7 @@ Some additional methods like `mersenne-primes` or `fermat-primes`
 To test whether number prime or not you have standard `prime?` predicate,
 works in O(sqrt(n)) time. Another primality tests are [TODO]
 
-# Generators
+# Generators [NEW]
 
 So, you want a random digit?
 
--- src/numberto/printers.clj
+++ src/numberto/printers.clj
@@ -1,5 +1,6 @@
 (ns numberto.printers
   (:require [numberto.converters :as c])
+  (:require [numberto.validator :as v])
   (:require [numberto.math :as m]))
 
 (def default-properties 
@@ -11,6 +12,7 @@
   "Print long number in short format"
   ([num] (format-num num default-properties))
   ([num props]
+     (v/validate num :integer)
      (let [{s :s e :e cnt :cnt} props
            num-seq (c/num->digits num) n (count num-seq)]
        (if (< (+ s e) n)
@@ -20,8 +22,65 @@
               (apply str (drop (- n e) num-seq)))
          (str num)))))
 
+(def number-names 
+  {1 "one" 2 "two" 3 "three" 4 "four" 5 "five" 6 "six" 7 "seven"
+   8 "eight" 9 "nine" 10 "ten" 11 "eleven" 12 "twelve" 13 "thirteen"
+   14 "fourteen" 15 "fifteen" 16 "sixteen" 17 "seventeen" 18 "eighteen"
+   19 "nineteen" 20 "twenty" 30 "thirty" 40 "forty" 50 "fifty"
+   60 "sixty" 70 "seventy" 80 "eighty" 90 "ninety" 100 "hundred"})
+
+;; number means *3 zeros
+(def big-names
+  {1 "thousand" 2 "million" 3 "billion" 4 "trillion" 5 "quadrillion"
+   6 "quintillion" 7 "sextillion" 8 "septillion" 9 "octillion"
+   10 "ninillion" 11 "decillion" 12 "undecillion" 13 "duodecillion"
+   14 "tredecillion" 15 "quattuordecillion" 16 "quindecillion"
+   17 "sexdecillion" 18 "septendecillion" 19 "octodecillion" 
+   20 "novemdecillion" 21 "vigintillion" 22 "unvigintillion"
+   23 "duovigintillion" 24 "trevigintillion" 25 "quattuorvigintillion"
+   26 "quinvigintillion" 27 "sexvigintillion" 28 "septenvigintillion"
+   29 "octovigintillion" 30 "novemvigintillion" 31 "trigintillion"
+   32 "untrigintillion" 33 "duotrigintillion"})
+
+(defn number-name [num]
+  "Convert number to english word representation. 
+   10^102 max number supported."
+  (v/validate num :integer #(< (m/count-digits %) 102))
+  (letfn [(closest [num]
+            (->> (keys number-names)
+                 sort
+                 reverse
+                 (drop-while #(> % num))
+                 first))
+          (less100 [num hundred] ;; merge 100 + 1000
+            (loop [n num s hundred]
+              (if (zero? n) (apply str (interpose " " s))
+                  (let [close (closest n)]
+                    (recur (- n close) (conj s (get number-names close)))))))
+          (less1000 [num]
+            (let [k (quot num 100)]
+              (if (zero? k) (less100 num [])
+                  (less100 (mod num 100) [(get number-names k)
+                                          (get number-names 100)]))))
+          (bignum-indexed [i num]
+            (let [s (less1000 num)]
+              (if (pos? i) 
+                (if-not (empty? s) (str s " " (get big-names i)))
+                (if-not (empty? s) s))))]
+    (->> num
+         c/num->digits
+         reverse
+         (partition 3 3 [0 0 0])
+         (map #(c/digits->num (reverse %)))
+         (map-indexed bignum-indexed)
+         reverse
+         (remove nil?)
+         (interpose " ")
+         (apply str))))
+
 (defn what [num]
   "Extract some useful number properties"
+  (v/validate num :integer)
   {:number num
    :count-of-digits (m/count-digits num)
    :sum-of-digits (m/sum-of-digits num)
