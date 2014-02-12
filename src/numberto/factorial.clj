6b714059fa0cd50086d14376e319f73475f2cb7b
mishadoff <mishadoff@gmail.com>
2014-02-13 01:48:10 +0200

    Stirling approximation draft

--- src/numberto/factorial.clj
+++ src/numberto/factorial.clj
@@ -24,3 +24,24 @@
                 (take-while #(<= % n) (p/primes)))
            acc 1]
       (if h (recur t (*' h acc)) acc))))
+
+(def ^:private ROUND java.math.RoundingMode/HALF_EVEN)
+
+(defn !-apx [n]
+  (letfn [(scale [d s] (.setScale d s ROUND))
+          (pow [x n]
+             (cond (= 0 n) 1
+                   (= 1 n) x
+                   (even? n) (pow (scale (*' x x) 10) (/ n 2))
+                   (odd? n) (*' x (pow (scale (*' x x) 10) (/ (dec n) 2)))))]
+    "Stirling approximation of n!"
+    (scale
+     (* (scale (bigdec (Math/sqrt (* 2 Math/PI n))) 10)
+        (pow (scale (bigdec (+ (/ n Math/E)
+                        (/ 1 (* 12 Math/E n)))) 10) n)) 0)))
+
+;; !-apx 7500 ms (m/power* accumulate scale 20)
+;; !-apx 2700 ms (m/power* accumulate scale 10)
+;; !-apx 560 ms (pow without accumulate scale 10)
+;; ! 750 ms
+;; !! 650 ms
--- src/numberto/irrational.clj
+++ src/numberto/irrational.clj
@@ -5,10 +5,20 @@
 ;; -- Metallica, Fuel
 
 (defn e [& {:keys [iterations limit] 
-                  :or {iterations 100 limit 16}}]
+            :or {iterations 100 limit 16}}]
+  "Expansion of formula 1 + E(1/n!)"
   (->> (range 1 (inc iterations))
        (reductions *')
        (map #(/ 1 %))
        (reduce +')
        (inc)
        (#(p/format-ratio % limit))))
+
+;; Archimedes method depends on sin and rough PI value
+;;;;  We can use taylor series for sin approximation 
+;; Ramanujan's method depends on sqrt(2) which is also irrational
+;; Arctan approximation slow and inaccurate
+
+(defn pi [] nil)
+
+(defn sqrt2 [] nil)
