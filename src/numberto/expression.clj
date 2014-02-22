ec235b9cc9712a044c94d1e4b8f18d635eba62ef
mishadoff <mishadoff@gmail.com>
2014-02-22 21:39:55 +0200

    Dynamic variables changed to atoms, Parsing function calls and symbols

--- src/numberto/expression.clj
+++ src/numberto/expression.clj
@@ -2,18 +2,21 @@
   (:require [numberto.math :as m]))
 
 ;; set to true if you want to track parsing process
-(def ^:dynamic *DEBUG* false)
+(def *DEBUG* (atom false))
 
 ;; can be replaced if you want to tweak algorithm
 ;; change priority, or add new functions
-(def ^:dynamic op-table
-  {"+" {:priority 1 :function + :assoc :left}
-   "-" {:priority 1 :function - :assoc :left}
-   "*" {:priority 2 :function * :assoc :left}
-   "/" {:priority 2 :function / :assoc :left}
-   "^" {:priority 3 :function m/power* :assoc :right}
-   }
-  )
+(def op-table
+  (atom
+   {"+" {:priority 1 :function + :assoc :left}
+    "-" {:priority 1 :function - :assoc :left}
+    "*" {:priority 2 :function * :assoc :left}
+    "/" {:priority 2 :function / :assoc :left}
+    "^" {:priority 3 :function m/power* :assoc :right}
+    }))
+
+(defn- parse-error []
+  (throw (IllegalArgumentException. "Invalid expression")))
 
 (def ^:private regexp-esc-map
   "regexp escape map"
@@ -24,77 +27,113 @@
 
 (defn- tokenize [expr]
   "split expression into list of known tokens"
-  (->> (keys op-table) ;; building tokenization regexp
+  (->> (keys @op-table) ;; building tokenization regexp
        (interpose "|") ;; based on op-table var
        (apply str)
        (replace regexp-esc-map)
-       (apply str "\\d+|\\(|\\)|") ;; adding numbers and parens
+       (apply str "\\w+|\\,|\\d+|\\(|\\)|") ;; adding numbers and parens
        (re-pattern)
        (#(re-seq % expr))))
 
-(defn- tag [token]
-  "Tag the token type.
-Available types: [:number :left-paren :right-paren :function]
+(defn- tag [tokens]
+  "Tag the tokens type. Almost all tokens can be tagged
+independently, except ones that need context.
+Context captured for left and right neighbour.
+Available types:
+[:number :left-paren :right-paren :function :funcall]
 Returns the triple [original value, tag, real value]"
-  (cond (= (count (re-find #"\d+" token)) (count token))
-        [token :number (bigint token)]
-        (= "(" token) [token :left-paren \(]
-        (= ")" token) [token :right-paren \)]
-        (op-table token)
-        [token :function (op-table token)]
-        :else (throw (IllegalArgumentException. (str "Unsupported token [" token "]")))))
+  (map
+   (fn [[left token right]]
+     (cond (= (count (re-find #"\d+" token)) (count token))
+           [token :number (bigint token)]
+           (= "(" token) [token :left-paren \(]
+           (= ")" token) [token :right-paren \)]
+           (= "," token) [token :arg-separator \,]
+           (@op-table token)
+           [token :op (@op-table token)]
+           (= (count (re-find #"\w+" token)) (count token))
+           (cond (= "(" right)
+                 [token :function token]
+                 :else [token :symbol token])
+           :else (throw (IllegalArgumentException. (str "Unsupported token [" token "]")))))
+   (partition 3 1 (concat [:gap] tokens [:gap]))))
 
-(defn parse [e]
-  "Parse and evaluates expression.
+(defn- infix->postfix [e]
+  "Parse infix expression into Reverse Polish Notation.
 Shunting-Yard algorithm
-Currently +, -, *, / and parens are supported
+Supported operations defined in the atom @op-table
 "
-  ;; TODO build tokenizator pattern dynamically based on settings
-  (let [tokens (vec (map tag (tokenize e)))
+  (let [tokens (vec (tag (tokenize e)))
         limit (dec (count tokens))]
-    (when *DEBUG*
+    (when @*DEBUG*
       (println "Input:" e)
       (println "Tagged Tokens:" tokens)
       (println "----------------------"))
     (loop [p 0 output [] opstack (list)]
-      (when *DEBUG*
+      (when @*DEBUG*
         (println "Output:" output)
         (println "Opstack:" opstack))
       (if (> p limit)
         ;; No more tokens
         (if (empty? opstack) output
-            (recur p (conj output (peek opstack)) (pop opstack)))
+            (recur p (conj output (first (peek opstack))) (pop opstack)))
         ;; Token process
         (let [[op1 tag props :as triple] (nth tokens p)]
-          (when *DEBUG*
+          (when @*DEBUG*
             (println "Token: " triple)
             (println "----------------------"))
           (cond (= tag :number)
                 (recur (inc p) (conj output op1) opstack)
+                (= tag :symbol)
+                (recur (inc p) (conj output op1) opstack)
                 (= tag :function)
-                (let [op2 (peek opstack) props2 (op-table op2)
+                (recur (inc p) output (conj opstack triple))
+                (= tag :arg-separator)
+                (let [[op2 tag2 _ :as all2] (peek opstack)]
+                  (when-not all2 (parse-error)) ;; empty stack
+                  (if (= :left-paren tag2)
+                    (recur (inc p) output opstack)
+                    (recur p (conj output op2) (pop opstack))))
+                (= tag :op)
+                (let [[op2 _ _ :as all2] (peek opstack) props2 (@op-table op2)
                       [p1 p2] (map :priority [props props2])]
                   (if (and props2 (or (< p1 p2) (and (= :left (:assoc props)) (= p1 p2))))
                     (recur p (conj output op2) (pop opstack))
-                    (recur (inc p) output (conj opstack op1))))
+                    (recur (inc p) output (conj opstack triple))))
                 (= tag :left-paren)
-                (recur (inc p) output (conj opstack tag))
+                (recur (inc p) output (conj opstack triple))
                 (= tag :right-paren) ;; optimize ;; handle mismatches
-                (let [op2 (peek opstack)]
-                  (if (= :left-paren op2)
-                    (recur (inc p) output (pop opstack))
+                (let [[op2 tag2 _ :as all2] (peek opstack)]
+                  (when-not all2 (parse-error)) ;; empty stack
+                  (if (= :left-paren tag2)
+                    (if (and (second opstack)
+                             (= :function (second (second opstack))))
+                      (recur (inc p) (conj output (first (second opstack))) (pop (pop opstack)))
+                      (recur (inc p) output (pop opstack)))
                     (recur p (conj output op2) (pop opstack))))
                 :else (throw (IllegalArgumentException. "Not supported yet"))))))))
 
-(defn eval-rpn [rpn]
-  (loop [[[_ t value :as all] & tokens] (map tag rpn) stack (list)]
+(defn- eval-postfix [expr]
+  "Evaluates postfix expression. 1 2 + => 3"
+  (loop [[[_ t value :as all] & tokens] (map tag expr)
+         stack (list)]
     (if all
       (cond (= :number t) (recur tokens (conj stack value))
-            (= :function t) (recur tokens
-                                   (conj (drop 2 stack) ;; invalid rpn
+            (= :op t) (recur tokens
+                                   (conj (drop 2 stack)
                                          (apply (:function value) (reverse (take 2 stack)))))
             :else (throw (IllegalArgumentException. "Invalid RPN")))
-      (first stack)) ;; if more in stack
+      (first stack))))
+
+(defn eval-infix [expr]
+  "Evaluate infix expression"
+  (eval-postfix (infix->postfix expr)))
+
+(defn infix->prefix [expr]
+  "Converts infix expression to lisp-style prefix expression
+   Example: a + b * c => (+ a (* b c))"
+  (let [postfix (tag (infix->postfix expr))]
+    
     ))
 
 ;; DONE Associativity
@@ -102,7 +141,6 @@ Currently +, -, *, / and parens are supported
 ;; TODO infix errors
 ;; TODO rpn errors
 ;; TODO unbalanced parens
-;; Right-to-Left
 ;; TODO prefix
 ;; TODO Debug
 ;; TODO custom functions
