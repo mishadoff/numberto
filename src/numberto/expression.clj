774c901c04cb82af1ada2c82c9a7d11dcd863826
mishadoff <mishadoff@gmail.com>
2014-02-22 22:16:35 +0200

    Ability to evaluate symbolic and function expression with provided bindings

--- src/numberto/expression.clj
+++ src/numberto/expression.clj
@@ -18,6 +18,10 @@
 (defn- parse-error []
   (throw (IllegalArgumentException. "Invalid expression")))
 
+(defn- eval-error [symbol]
+  (throw (IllegalArgumentException. (str "Binding for [" symbol "] is not provided"))))
+
+
 (def ^:private regexp-esc-map
   "regexp escape map"
   (let [esc-chars "()[]{}*+/&^$%#!"]
@@ -76,16 +80,18 @@ Supported operations defined in the atom @op-table
       (if (> p limit)
         ;; No more tokens
         (if (empty? opstack) output
-            (recur p (conj output (first (peek opstack))) (pop opstack)))
+            (let [[_ tag _ :as triple] (peek opstack)]
+              (when (= tag :left-paren) (parse-error))
+              (recur p (conj output triple) (pop opstack))))
         ;; Token process
         (let [[op1 tag props :as triple] (nth tokens p)]
           (when @*DEBUG*
             (println "Token: " triple)
             (println "----------------------"))
           (cond (= tag :number)
-                (recur (inc p) (conj output op1) opstack)
+                (recur (inc p) (conj output triple) opstack)
                 (= tag :symbol)
-                (recur (inc p) (conj output op1) opstack)
+                (recur (inc p) (conj output triple) opstack)
                 (= tag :function)
                 (recur (inc p) output (conj opstack triple))
                 (= tag :arg-separator)
@@ -93,12 +99,12 @@ Supported operations defined in the atom @op-table
                   (when-not all2 (parse-error)) ;; empty stack
                   (if (= :left-paren tag2)
                     (recur (inc p) output opstack)
-                    (recur p (conj output op2) (pop opstack))))
+                    (recur p (conj output all2) (pop opstack))))
                 (= tag :op)
                 (let [[op2 _ _ :as all2] (peek opstack) props2 (@op-table op2)
                       [p1 p2] (map :priority [props props2])]
                   (if (and props2 (or (< p1 p2) (and (= :left (:assoc props)) (= p1 p2))))
-                    (recur p (conj output op2) (pop opstack))
+                    (recur p (conj output all2) (pop opstack))
                     (recur (inc p) output (conj opstack triple))))
                 (= tag :left-paren)
                 (recur (inc p) output (conj opstack triple))
@@ -108,31 +114,44 @@ Supported operations defined in the atom @op-table
                   (if (= :left-paren tag2)
                     (if (and (second opstack)
                              (= :function (second (second opstack))))
-                      (recur (inc p) (conj output (first (second opstack))) (pop (pop opstack)))
+                      (recur (inc p) (conj output (second opstack)) (pop (pop opstack)))
                       (recur (inc p) output (pop opstack)))
-                    (recur p (conj output op2) (pop opstack))))
+                    (recur p (conj output all2) (pop opstack))))
                 :else (throw (IllegalArgumentException. "Not supported yet"))))))))
 
-(defn- eval-postfix [expr]
+(defn- eval-postfix [tagged-tokens bindings]
   "Evaluates postfix expression. 1 2 + => 3"
-  (loop [[[_ t value :as all] & tokens] (map tag expr)
-         stack (list)]
-    (if all
-      (cond (= :number t) (recur tokens (conj stack value))
-            (= :op t) (recur tokens
-                                   (conj (drop 2 stack)
-                                         (apply (:function value) (reverse (take 2 stack)))))
+  (loop [[[token tag value :as triple] & tokens]
+         tagged-tokens stack (list)]
+    (if triple
+      (cond (= :number tag) (recur tokens (conj stack value))
+            (= :symbol tag)
+            (let [value (bindings token)]
+              (if-not value (eval-error token))
+              (recur tokens (conj stack (bindings token))))
+            (= :op tag) (recur tokens (conj (drop 2 stack)
+                                          (apply (:function value) (reverse (take 2 stack)))))
+            (= :function tag)
+            (let [[f arity :as b] (bindings token)]
+              (if-not b (eval-error token))
+              (recur tokens (conj (drop arity stack)
+                                  (apply f (reverse (take arity stack))))))
             :else (throw (IllegalArgumentException. "Invalid RPN")))
       (first stack))))
 
-(defn eval-infix [expr]
-  "Evaluate infix expression"
-  (eval-postfix (infix->postfix expr)))
+(defn eval-infix 
+  "Evaluate infix expression.
+If functions or symbols are used, provide bindings map
+"
+  ([expr]
+     (eval-infix expr {}))
+  ([expr bindings]
+     (eval-postfix (infix->postfix expr) bindings)))
 
 (defn infix->prefix [expr]
   "Converts infix expression to lisp-style prefix expression
    Example: a + b * c => (+ a (* b c))"
-  (let [postfix (tag (infix->postfix expr))]
+  (let [postfix (infix->postfix expr)]
     
     ))
 
@@ -142,7 +161,6 @@ Supported operations defined in the atom @op-table
 ;; TODO rpn errors
 ;; TODO unbalanced parens
 ;; TODO prefix
-;; TODO Debug
 ;; TODO custom functions
 ;; TODO Arity
 ;; TODO clean code
