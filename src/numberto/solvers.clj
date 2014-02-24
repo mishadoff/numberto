87de52693ab7676d7589a1081d6045906461c74b
mishadoff <mishadoff@gmail.com>
2014-02-25 00:36:21 +0200

    Permutation ops solver implemented

--- src/numberto/solvers.clj
+++ src/numberto/solvers.clj
@@ -1,8 +1,7 @@
 (ns numberto.solvers
   (:require [numberto.converters :as c])
+  (:require [numberto.expression :as e])
   (:require [numberto.math :as m]))
-
-(def ^:private ops [+ - * /])
 
 (defn- binary-split [numbers code]
   "Build splits from numbers and binary code indicates gaps
@@ -15,15 +14,38 @@ binary-split [1 2 3 4] [:gap :none :gap]) => [1 23 4]"
        (remove empty?)
        (map c/digits->num)))
 
+(defn fill-zeros [n]
+  (partial format (str "%0" n "d")))
+
 (defn- splits [numbers]
   (let [n (dec (count numbers))
-        fill-zeros (partial format (str "%0" n "d"))
         limit (m/power* 2 n)]
     (for [i (range limit)]
       (->> (str i)
            (#(c/radix-convert % 10 2))
            (biginteger)
-           (fill-zeros)
+           ((fill-zeros n))
            (seq)
            (map #({\0 :none \1 :gap} %))
            (binary-split numbers)))))
+
+(defn- permute-ops [split]
+  (let [n (count split)]
+    (if (= 1 n) [(str split)]
+        (for [i (range (m/power* 4 (dec n)))]
+          (->> (str i)
+               (#(c/radix-convert % 10 4))
+               (biginteger)
+               ((fill-zeros (dec n)))
+               (seq)
+               (map #({\0 '+ \1 '- \2 '* \3 '/} %))
+               (cons nil)
+               (#(interleave % split))
+               (rest)
+               (apply str))))))
+
+(defn insert-ops-solver [numbers]
+  (map #(vec [(e/eval-infix %) %]) (permute-ops numbers)))
+
+;; TODO Customizable operations
+;; TODO Parens handler
