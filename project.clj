06c8798d5908d200daa8fc4741acf88eac16737f
Mykhailo Kozik <misha@zoomdata.com>
2016-02-02 13:42:39 -0500

    Added dependency for cloverage

--- project.clj
+++ project.clj
@@ -3,4 +3,8 @@
   :url "https://github.com/mishadoff/numberto"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :profiles {:dev {:plugins [[jonase/eastwood "0.2.1"]
+                             [lein-kibit "0.1.2"]
+                             [lein-bikeshed "0.2.0"]
+                             [lein-cloverage "1.0.6"]]}})
